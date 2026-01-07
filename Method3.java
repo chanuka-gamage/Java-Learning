957d3c3f8d3384e8b12ef5791d1c266b318bc034
Chanuka Gamage <shashiduchanuka@gmail.com>
2026-01-07 20:14:02 +0530

    Method 3

---
+++ Method3.java
@@ -0,0 +1,14 @@
+public class Method3{
+	public void fl(){
+		System.out.println("The car is going as fast as it can!");
+	}
+	public void sd(int maxSpeed){
+		System.out.println("Max speed is :"+maxSpeed);
+	}
+	public static void main (String[]args){
+		Method3 mycar=new Method3();
+		mycar.fl();
+		mycar.sd(200);
+	}
+	}
+		
